e99f66e7f5fb1860dcf645b85ac4b1529f5591bb
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-10-13 16:29:16 -0300

    produzindo um dataframe utilizando web scrapping

--- app.py
+++ app.py
@@ -1,77 +1,103 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 
 url = 'https://sofifa.com/'
 browser={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
 
-page= requests.get(url,headers=browser)
-response=page.text
+# page= requests.get(url,headers=browser)
+# response=page.text
 
-soup=BeautifulSoup(response,'html.parser')
+# soup=BeautifulSoup(response,'html.parser')
 
+url_ids = np.arange(0,20000,60).tolist() #GERANDO ID'S PARA A URL
+url_list=[]
 list_player= []
-cont=0
 
-for line in soup.find_all('tr'):
-    
-    if cont > 1:
-        
-        dict_player= {}
-        
-        ref_player = line.find('td', class_='col-name')
-        
-        #ID JOGADOR
-        dict_player['Id'] = ref_player.a.get('href').split('/')[2]
+for url_id in url_ids:
+    url_with_id = f'{url}?offset={url_id}'
+    url_list.append(url_with_id)
 
-        #NOME JOGADOR
-        dict_player['Nome'] = ref_player.a.text
+for url_id in url_list:
+    page = requests.get(url_id,headers=browser)
+    response=page.text
+    soup=BeautifulSoup(response,'html.parser')
+
+    cont=0
+
+    for line in soup.find_all('tr'):
         
-        #POSIÇÃO JOGADOR
-        player_positions=ref_player.find_all('span')
-        positions=[]
-        for position in range(len(player_positions)):
-            if len(positions) <= len(player_positions):
-                positions.append(player_positions[position].text)
-                dict_player['Posições']= positions
-            else: break
+        # if (cont % 60) == 0:
+        #     print('\n')
+        #     print(cont)
+        
+        if cont > 0:
             
-        #NACIONALIDADE (BANDEIRA)
-        dict_player['Nacionalidade']=ref_player.find('img').get('data-src')
-        
-        #IDADE
-        dict_player['Idade']=line.find('td',class_='col col-ae').text
-        
-        #OVER
-        dict_player['Overral']=line.find('td',class_='col col-oa').text
-        
-        #POTENTIAL
-        dict_player['Potential']=line.find('td',class_='col col-pt').text
+            dict_player= {}
+            
+            ref_player = line.find('td', class_='col-name')
+            
+            #ID JOGADOR
+            dict_player['Id'] = ref_player.a.get('href').split('/')[2]
 
-        #TIME E CONTRATO
-        team_contrat=line.find_all('td', class_='col-name')[1]
+            #NOME JOGADOR
+            dict_player['Nome'] = ref_player.a.text
+            
+            #POSIÇÃO JOGADOR
+            player_positions=ref_player.find_all('span')
+            positions=[]
+            [positions.append(player_positions[position].text) for position in range(len(player_positions))]
+            dict_player['Posições']=str(positions)
+            
+            #NACIONALIDADE (BANDEIRA)
+            dict_player['Nacionalidade']=ref_player.find('img').get('data-src')
+            
+            #IDADE
+            dict_player['Idade']=line.find('td',class_='col col-ae').text
+            
+            #OVER
+            dict_player['Overral']=line.find('td',class_='col col-oa').text
+            
+            #POTENTIAL
+            dict_player['Potential']=line.find('td',class_='col col-pt').text
+
+            #TIME E CONTRATO
+            team_contrat=line.find_all('td', class_='col-name')[1]
+            
+            dict_player['Time']=team_contrat.find('a').text
+            dict_player['Time(Bandeira)']=line.find_all('td', class_='col-name')[1].find('img').get('data-src')
+            
+            dict_player['Tempo de Contrato']=team_contrat.find('div',class_='sub').text.split('\n')[1]
+            
+            #VALOR
+            dict_player['Valor']=line.find('td', class_='col col-vl').text
+            
+            #SALARIO
+            dict_player['Salário']=line.find('td',class_='col col-wg').text
+            
+            #STATUS TOTAIS
+            dict_player['Status totais']=line.find('td','col col-tt').text
+            
+            list_player.append(dict_player)
+            
+        cont +=1
         
-        dict_player['Time']=team_contrat.find('a').text
-        dict_player['Time(Bandeira)']=line.find_all('td', class_='col-name')[1].find('img').get('data-src')
-        
-        dict_player['Tempo de Contrato']=team_contrat.find('div',class_='sub').text.split('\n')[1]
-        
-        #VALOR
-        dict_player['Valor']=line.find('td', class_='col col-vl').text
-        
-        #SALARIO
-        dict_player['Salário']=line.find('td',class_='col col-wg').text
-        
-        #STATUS TOTAIS
-        dict_player['Status totais']=line.find('td','col col-tt').text
-        
-        list_player.append(dict_player)
-        
-        df = pd.DataFrame(list_player)
-        df
-    
-    cont +=1
-        
+df = pd.DataFrame(list_player)
+df['Posições'] = df['Posições'].str.replace('[', '').str.replace(']', '').str.replace("'",'')
+
+df.to_csv('jogadores_eafc24',sep=';',index=False,encoding='utf-8')
+
+
+
+
+
+
+
+
+
+
+
 
 
     
